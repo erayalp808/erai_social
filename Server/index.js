8a2b80024ace8ccee4813524f9df0ff5b8caf4ad
Eray Alp <erayalp.e@gmail.com>
2023-12-14 12:55:17 +0300

    error fixed

--- Server/index.js
+++ Server/index.js
@@ -44,5 +44,3 @@ const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${ PORT }`))
 }).catch((error) => console.log(`${ error } did not connect`));
-
-register()

64e03f78225130e7053e0f0c9fcf7d363defc83f
Eray Alp <erayalp.e@gmail.com>
2023-12-15 17:23:23 +0300

    user operation routes added

---
+++ Server/routes/users.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+    getUser,
+    getUserFriends,
+    addRemoveFriend
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.use("/:id", verifyToken, getUser);
+router.use("/:id/friends", verifyToken, getUserFriends);
+
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);

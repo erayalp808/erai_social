3b5d5bfce799edb3221568756efc18951fa0304c
Eray Alp <erayalp.e@gmail.com>
2023-12-15 17:38:24 +0300

    getUserFriends method added

--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -10,3 +10,22 @@ export const getUser = async (request, response) => {
         response.status(404).json({ errorMessage: error.message });
     }
 }
+
+export const getUserFriends = async (request, response) => {
+    try {
+        const { id } = request.params;
+        const user = await User.findById(id);
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+
+        const formattedFriends = friends.map(
+            ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+                return { _id, firstName, lastName, occupation, location, picturePath };
+            });
+        response.status(200).json(formattedFriends);
+    } catch (error) {
+        response.status(404).json({ errorMessage: error.message });
+    }
+}

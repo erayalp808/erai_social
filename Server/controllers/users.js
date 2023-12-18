85ea6fdb657796872d4ca5ff9fdfd9f1057b0616
Eray Alp <erayalp.e@gmail.com>
2023-12-18 18:49:24 +0300

    add/remove friend function added

--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -29,3 +29,35 @@ export const getUserFriends = async (request, response) => {
         response.status(404).json({ errorMessage: error.message });
     }
 }
+
+/* UPDATE */
+export const addRemoveFriend = async (request, response) => {
+    try {
+        const { id, friendId} = request.params;
+        const user = await User.findById(id);
+        const friend = await User.findById(friendId);
+
+        if (user.friends.includes(friend)) {
+            user.friends = user.friends.filter((id) => id !== friendId);
+            friend.friends = friend.friends.filter((friendId) => friendId !== id);
+        } else {
+            user.friends.push(friendId);
+            friend.friends.push(id);
+        }
+
+        await user.save();
+        await friend.save();
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+
+        const formattedFriends = friends.map(
+            ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+                return { _id, firstName, lastName, occupation, location, picturePath };
+            });
+        response.status(200).json(formattedFriends);
+    } catch (error) {
+        response.status(404).json({ errorMessage: error.message });
+    }
+}

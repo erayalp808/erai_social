d9eb57a15890bcb30897f90bd3aad6ecfda9cd58
Eray Alp <erayalp.e@gmail.com>
2023-12-22 10:49:20 +0300

    likePost function added

--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -47,3 +47,29 @@ export const getUserPosts = async (request, response) => {
         response.status(404).json({ errorMessage: error.message });
     }
 }
+
+/* UPDATE */
+export const likePost = async (request, response) => {
+    try {
+        const { id } = request.params;
+        const { userId } = request.body;
+        const post = await Post.findById(id);
+        const isLiked = post.likes.get(userId);
+
+        if (isLiked) {
+            post.likes.delete(userId);
+        } else {
+            post.likes.set(userId, true);
+        }
+
+        const updatedPost = await Post.findByIdAndUpdate(
+            id,
+            { likes: post.likes },
+            { new: true },
+        )
+
+        response.status(200).json(updatedPost);
+    } catch (error) {
+        response.status(404).json({ errorMessage: error.message });
+    }
+}
